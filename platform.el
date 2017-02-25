a4dca7070daead05c1c14fc2d2efc4a56a68588e
esullivan <eamonn.sullivan@gmail.com>
2017-02-25 15:08:51 +0000

    Move all Mac-specific code into the same block

--- platform.el
+++ platform.el
@@ -1,5 +1,6 @@
 ;; Handle emacs server interactions on the Mac correctly.
 (when (featurep 'ns)
+  (message "Running mac-specific initialization.")
   (defun ns-raise-emacs ()
     "Raise Emacs."
     (ns-do-applescript "tell application \"Emacs\" to activate"))
@@ -13,19 +14,17 @@
   (add-hook 'after-make-frame-functions 'ns-raise-emacs-with-frame)
 
   (when (display-graphic-p)
-    (ns-raise-emacs)))
+    (ns-raise-emacs))
 
-;; use local bin, if available
-(if (file-directory-p "/usr/local/bin")
-    (add-to-list 'exec-path "/usr/local/bin"))
-(if (file-directory-p "/usr/local/bin")
-    (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin")))
-
-(when (memq window-system '(mac ns))
   (use-package exec-path-from-shell
     :ensure t
     :config
     (message "Initializing path from shell on Mac OS")
     (exec-path-from-shell-initialize)))
 
+;; use local bin, if available
+(if (file-directory-p "/usr/local/bin")
+    (add-to-list 'exec-path "/usr/local/bin"))
+(if (file-directory-p "/usr/local/bin")
+    (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin")))
 
