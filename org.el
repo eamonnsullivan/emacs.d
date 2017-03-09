5da8a1b9a66720c952388cc4fca854317c4fe8cc
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2017-03-09 11:11:10 +0000

    capture template work

--- org.el
+++ org.el
@@ -2,6 +2,10 @@
 
 (defvar org-directory)
 (defvar org-default-notes-file)
+(defvar org-capture-templates)
 (setq org-directory "~/git/org")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
-
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline (concat org-directory "/notes.org") "Tasks"))
+        ("i" "Ideas" entry (file+headline (concat org-directory "/notes.org") "Ideas"))
+        ("n" "Notes" entry (file+headline (contact org-directory "/notes.org") "General Notes"))))
