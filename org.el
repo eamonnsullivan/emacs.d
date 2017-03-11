d8e609a9a2c1c01c5bf80c10b7ab9c1bd0dc4094
Eamonn Sullivan <eamonn.sullivan@gmail.com>
2017-03-11 10:28:27 +0000

    bullets in org mode

--- org.el
+++ org.el
@@ -9,3 +9,8 @@
       '(("t" "Todo" entry (file+headline (concat org-directory "/notes.org") "Tasks"))
         ("i" "Ideas" entry (file+headline (concat org-directory "/notes.org") "Ideas"))
         ("n" "Notes" entry (file+headline (concat org-directory "/notes.org") "General Notes"))))
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
