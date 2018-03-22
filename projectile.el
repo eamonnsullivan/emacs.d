bb872a72213b3e5ae801c530b2e8db1bb635c0bd
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2018-03-22 14:33:44 +0000

    Undo that last change. Removing slow native indexing on projectile.

--- projectile.el
+++ projectile.el
@@ -27,7 +27,6 @@
   :diminish projectile-mode
   :init
   (setq projectile-use-git-grep t)
-  (setq projectile-indexing-method 'native)
   :config
   (projectile-global-mode t)
   (helm-projectile-on)
