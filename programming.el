5a9cc37e0cd8c5bdc799f29d2955ad9f831223c4
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2018-05-03 09:48:30 +0100

    Try emr.

--- programming.el
+++ programming.el
@@ -394,4 +394,11 @@ class %TESTCLASS% extends FlatSpec with MustMatchers {
     :chords ((".." . dumb-jump-go)
              (",," . dumb-jump-back)))
 
+;; refactoring
+(use-package emr
+  :config
+  (add-hook 'prog-mode-hook 'emr-initialize)
+  ;; Just hit M-RET to access your refactoring tools in any supported mode.
+  (define-key prog-mode-map (kbd "M-RET") 'emr-show-refactor-menu))
+
 ;;; programming.el ends here
