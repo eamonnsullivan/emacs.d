c121a496eccd34a93b5e6c582c4d74d7bbc5eecb
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2018-05-24 15:37:14 +0100

    fix hydra menu so that it sticks around.

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -8,14 +8,14 @@
 
 (global-set-key
  (kbd "C-z")
- (defhydra "hydra-global-menu" (:color blue)
-   "global menu"
-  ("g" default-text-scale-increase "zoom-in")
-  ("s" default-text-scale-decrease "zoom-out")
-  ("d" kill-all-buffers "kill all buffers")
-  ("r" stop-and-restart-emacs "stop and restart emacs")
-  ("u" upgrade-packages "upgrade all packages")
-  ("q" nil "quit")))
+ (defhydra "hydra-global-menu" (:color red)
+   "My Global Menu"
+   ("g" default-text-scale-increase "zoom-in")
+   ("s" default-text-scale-decrease "zoom-out")
+   ("d" kill-all-buffers "kill all buffers")
+   ("r" stop-and-restart-emacs "stop and restart emacs")
+   ("u" upgrade-packages "upgrade all packages")
+   ("q" nil "quit")))
 (hydra-set-property 'hydra-global-menu :verbosity 1)
 
 (provide 'init-hydra)
