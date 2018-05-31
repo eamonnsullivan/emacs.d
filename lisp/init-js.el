40c888e0fe22bfd468859e72219cc904c750a3c7
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2018-05-31 17:05:27 +0100

    Some basic test features while working on JSX.

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -79,17 +79,60 @@
 
 
 ;; my own shortcuts
-(defun eds/insert-enzyme-test-case ()
-  (interactive)
+(defun eds/insert-enzyme-test-case (arg)
+  "Insert a skeleton test case at point. If a prefix is used, make it synchronous."
+  (interactive "P")
   (move-beginning-of-line 1)
-  (insert (format "test('%s', async () => {\n\n});" (read-string "Test description: "))))
+  (let ((description (read-string "Test description: "))
+        (function-signature (if arg "()" "async ()")))
+        (insert (format "test('%s', %s => {\n\n});" description function-signature))))
+
+(defun eds/insert-skeleton-test-file (export module-name)
+  "Insert a basic skeleton for a test file."
+  (goto-char (point-min))
+  (insert "import React from 'react';\n")
+  (insert "import { shallow } from 'enzyme';\n")
+  (insert (format "import %s from '%s'\n\n" export module-name))
+  (insert "test('', async () => {\n\n});\n"))
+
+(defun eds/find-default-export ()
+  "Try to find the default export for the current module."
+  (save-excursion
+    (goto-char (point-min))
+    (let ((pos (re-search-forward "^export default " nil t)))
+      (if pos
+          (let ((default-export (string-trim (buffer-substring-no-properties pos (point-max)))))
+            (if (string-match ";$" default-export)
+                (substring default-export 0 -1)
+              default-export))
+        "{ NoDefaultExportFound }"))))
+
+(defun eds/open-or-create-enzyme-test-file ()
+  "Open the current buffer's test file or create one if none is found."
+  (interactive)
+  (if (and buffer-file-name (equal (downcase (file-name-extension buffer-file-name)) "js"))
+      (if (string-match ".test.js$" buffer-file-name)
+          (message "This is already a test file. Exiting")
+        (let* ((basename (substring buffer-file-name 0 (string-match ".js$" buffer-file-name)))
+               (test-file (concat basename ".test.js"))
+               (module-name (file-name-nondirectory basename)))
+          (if (file-exists-p test-file)
+              (find-file test-file)
+            (progn
+              (let ((default-export (eds/find-default-export)))
+                (find-file test-file)
+                (rjsx-mode)
+                (eds/insert-skeleton-test-file default-export module-name))))))
+  (message "Only javascript files are supported at the moment.")))
 
 (require 'init-hydra)
 
 (defvar eds/javascript-macros
   (defhydra "hydra-my-javascript-macros" (:color blue)
-      ("t" eds/insert-enzyme-test-case "Insert an enzyme test case")
-      ("q" nil "quit")))
+    ("a" (eds/insert-enzyme-test-case nil) "Insert a test case")
+    ("s" (eds/insert-enzyme-test-case 't) "Insert a synchronous test case")
+    ("t" (eds/open-or-create-enzyme-test-file) "Open the test file for this module")
+    ("q" nil "quit")))
 
 (eval-after-load 'js2-mode
   '(key-chord-define js2-mode-map "MM" eds/javascript-macros))
