2f914dec375d99a87b913a2ac59992544c4564d6
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2018-10-20 21:06:31 +0100

    reformatting

--- lisp/eds.el
+++ lisp/eds.el
@@ -7,19 +7,35 @@
   "Insert a skeleton test case at point. If a prefix is used, make it synchronous."
   (interactive "P")
   (move-beginning-of-line 1)
-  (let ((description (read-string "Test description: "))
+  (let ((description (read-string
+                      "Test description: "))
         (function-signature (if arg "()" "async ()")))
-    (insert (format "test(\"%s\", %s => {\n\n});" description function-signature))
+    (insert
+     (format
+      "test(\"%s\", %s => {\n\n});"
+      description
+      function-signature))
     (forward-line -1)
     (indent-for-tab-command)))
 
 (defun eds/insert-skeleton-test-file (export module-name)
   "Insert a basic skeleton for a test file."
   (goto-char (point-min))
-  (insert "import React from 'react';\n")
-  (insert "import { shallow } from 'enzyme';\n")
-  (insert (format "import %s from './%s';\n\n" export module-name))
-  (insert "test('', async () => {\n\n});\n")
+  (insert
+   "import React from 'react';\n")
+  (insert
+   "import { shallow } from 'enzyme';
+")
+  (insert
+   (format
+    "import %s from './%s';\n\n"
+    export
+    module-name))
+  (insert
+   "test('', async () => {
+
+});
+")
   (forward-line -2)
   (indent-for-tab-command))
 
@@ -27,10 +43,18 @@
   "Try to find the default export for the current module."
   (save-excursion
     (goto-char (point-min))
-    (let ((pos (re-search-forward "^export default " nil t)))
+    (let ((pos (re-search-forward
+                "^export default "
+                nil
+                t)))
       (if pos
-          (let ((default-export (string-trim (buffer-substring-no-properties pos (point-max)))))
-            (if (string-match ";$" default-export)
+          (let ((default-export (string-trim
+                                 (buffer-substring-no-properties
+                                  pos
+                                  (point-max)))))
+            (if (string-match
+                 ";$"
+                 default-export)
                 (substring default-export 0 -1)
               default-export))
         "{ NoDefaultExportFound }"))))
@@ -39,30 +63,61 @@
   "Open the current buffer's test file or create one if none is found."
   (if (file-exists-p test-file)
       (find-file test-file)
-    (let* ((module-name (file-name-nondirectory (substring test-file 0 (string-match ".test.js" test-file))))
+    (let* ((module-name (file-name-nondirectory
+                         (substring
+                          test-file
+                          0
+                          (string-match
+                           ".test.js"
+                           test-file))))
            (default-export (eds/find-default-export)))
       (find-file test-file)
       (rjsx-mode)
-      (eds/insert-skeleton-test-file default-export module-name))))
+      (eds/insert-skeleton-test-file
+       default-export
+       module-name))))
 
 (defun eds/is-js (fn)
   "Is the file javascript?"
-  (and fn (equal (downcase (file-name-extension fn)) "js")))
+  (and fn
+       (equal (downcase
+               (file-name-extension fn))
+              "js")))
 
 (defmacro eds/when-file-is-js (fn foo)
   "Do something if filename is javascript."
-  `(if (and ,fn (equal (downcase (file-name-extension ,fn)) "js")) ,foo))
+  `(if
+       (and ,fn
+            (equal (downcase
+                    (file-name-extension ,fn))
+                   "js"))
+       ,foo))
 
 (defmacro eds/if-js-test (fn then else)
   "Do something if filename is a javascript test file or something else if it isn't."
   (declare (indent 2))
-  `(eds/when-file-is-js ,fn (if (string-match ".test.js$" ,fn) ,then ,else)))
+  `(eds/when-file-is-js
+    ,fn
+    (if (string-match ".test.js$" ,fn)
+        ,then
+      ,else)))
 
 (defun eds/get-test-or-impl (fn)
   "Get this file's corresponding test or implementation filename."
   (if (eds/is-js fn)
-      (let* ((basename (substring fn 0 (string-match (eds/if-js-test fn ".test.js$" ".js$") fn)))
-             (ext-to-add (eds/if-js-test fn ".js" ".test.js")))
+      (let* ((basename (substring
+                        fn
+                        0
+                        (string-match
+                         (eds/if-js-test
+                             fn
+                             ".test.js$"
+                           ".js$")
+                         fn)))
+             (ext-to-add (eds/if-js-test
+                             fn
+                             ".js"
+                           ".test.js")))
         (concat basename ext-to-add))
     fn))
 
@@ -85,41 +140,61 @@ When trying to open the test file, create a new test file if we can't find an ex
   "Insert the current git branch name at point, surrounded by square brackets.
 We use this to make the jira tickets easy to spot in the commit messages."
   (interactive)
-  (insert "[" (eds/extract-jira-ticket branch) "] "))
+  (insert
+   "["
+   (eds/extract-jira-ticket
+    branch)
+   "] "))
 
 (defun eds/kill-word (arg)
   "Delete characters forward until encountering the end of a word.
 With argument, do this that many times."
   (interactive "p")
   (if (use-region-p)
-      (delete-region (region-beginning) (region-end))
-    (delete-region (point) (progn (forward-word arg) (point)))))
-
-(defun eds/backward-kill-word (arg)
+      (delete-region
+       (region-beginning)
+       (region-end))
+    (delete-region
+     (point)
+     (progn
+       (forward-word arg)
+       (point)))))(defun eds/backward-kill-word (arg)
   "Delete characters backward until encountering the end of a word.
 With argument, do this that many times."
   (interactive "p")
   (eds/kill-word (- arg)))
 
 (defun eds/switch-to-previous-buffer ()
-    "Switch to previously open buffer.
+  "Switch to previously open buffer.
   Repeated invocations toggle between the two most recently open buffers."
-    (interactive)
-    (switch-to-buffer (other-buffer (current-buffer) 1)))
+  (interactive)
+  (switch-to-buffer
+   (other-buffer
+    (current-buffer)
+    1)))
 
 (defun eds/open-buffer-on-desktop ()
   "Open the current directory in Mac OS X finder, Nautilus or Explorer"
   (interactive)
   (let ((file (buffer-file-name))
-        (cmd (cond ((eq system-type 'darwin) "open")
-                   ((eq system-type 'gnu/linux) "nautilus")
-                   ((eq system-type 'windows-nt) "explorer") ;; haven't tested this
+        (cmd (cond ((eq system-type 'darwin)
+                    "open")
+                   ((eq system-type 'gnu/linux)
+                    "nautilus")
+                   ((eq system-type 'windows-nt)
+                    "explorer")
+                   ;; haven't tested this
                    (t nil))))
     (if file
         (if cmd
             (shell-command
-             (format "%s %s" (executable-find cmd) (file-name-directory file)))
-          (display-warning :error "Unknown system type"))
-      (display-warning :error "Buffer not attached to any file"))))
+             (format
+              "%s %s"
+              (executable-find cmd)
+              (file-name-directory file)))
+          (display-warning
+           :error "Unknown system type"))
+      (display-warning
+       :error "Buffer not attached to any file"))))
 
 (provide 'eds)
