519f262ae95d0bf8f866a3ff38c4b4d9a100ded8
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2020-04-03 17:21:07 +0100

    Use a different key for multiple cursors.

--- lisp/init-mc.el
+++ lisp/init-mc.el
@@ -4,8 +4,8 @@
 (use-package multiple-cursors
   :config
   (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-  (global-set-key (kbd "<s-down>") 'mc/mark-next-like-this)
-  (global-set-key (kbd "<s-up>") 'mc/unmark-next-like-this)
+  (global-set-key (kbd "<C-s-down>") 'mc/mark-next-like-this)
+  (global-set-key (kbd "<C-s-up>") 'mc/unmark-next-like-this)
   (global-unset-key (kbd "M-<down-mouse-1>"))
   (global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click))
 
