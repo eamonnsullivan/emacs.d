747c78a6573ea210671482528975fd8f1ea680a9
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2020-04-30 10:52:12 +0100

    Initial working slack setup.

--- lisp/init-slack.el
+++ lisp/init-slack.el
@@ -1,12 +1,25 @@
+(use-package helm-slack
+  :straight
+  (helm-slack :type git :host github :repo "yuya373/helm-slack")
+  :after (slack)
+  :bind (("<f6>" . helm-slack)))
+
 (use-package slack
-  ;; :secret
-  ;; (slack-start "~/work.el")
-  :commands
-  (slack-start)
-  :custom
-  (slack-buffer-emojify t)
-  (slack-prefer-current-team t)
-  (slack-redirect-url "http://localhost:8081"))
+  :commands (slack-start)
+  :init
+  (setq slack-buffer-emojify t)
+  (setq slack-prefer-current-team t)
+  :config
+  (slack-register-team
+   :name "BBC Digital Publishing"
+   :token (auth-source-pick-first-password
+           :host "bbcdigitalpublishing.slack.com"
+           :user "eamonn.sullivan@bbc.co.uk"))
+  (slack-register-team
+   :name "Clojurians"
+   :token (auth-source-pick-first-password
+           :host "clojurians.slack.com"
+           :user "eamonn.sullivan@bbc.co.uk")))
 
 (use-package alert
   :commands (alert)
