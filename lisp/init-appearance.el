4d81b98c2f9a1024d6391ad406b4a5ac22f72532
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2024-04-07 19:27:03 +0100

    Tweaks to the agenda view.

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -11,7 +11,15 @@
   :straight
   (modus-themes :type git :host gitlab :repo "protesilaos/modus-themes" :branch "main")
   :init
-  (setq modus-themes-mixed-fonts t)
+  (setq modus-themes-mixed-fonts t
+        modus-themes-common-palette-overrides '((date-common cyan)   ; default value (for timestamps and more)
+                                                (date-deadline red-warmer)
+                                                (date-event magenta-warmer)
+                                                (date-holiday blue) ; for M-x calendar
+                                                (date-now yellow-warmer)
+                                                (date-scheduled magenta-cooler)
+                                                (date-weekday cyan-cooler)
+                                                (date-weekend blue-faint)))
   :config
   (modus-themes-load-theme 'modus-operandi-tinted)
   :demand t)
