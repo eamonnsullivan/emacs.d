e43785580b84ad0949ade66b2a0390ea482d750b
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2024-12-24 17:28:46 +0000

    Leave it at the defaults for now, until I learn more.

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -2,8 +2,6 @@
 ;;; init-eglot.el --- stuff related to the language server protocol
 
 (use-package eglot
-  :config
-  (setq eglot-autoshutdown t)
   :hook ((prog-mode . eglot-ensure)))
 
 (provide 'init-eglot)
