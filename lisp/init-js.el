2a54be1b1b6ace80e53ffaad7eea6cc808e00926
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2024-12-27 12:18:20 +0000

    Fix void function reference, simplify Javascript/Typescript modes

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -1,19 +1,10 @@
 ;;; -*- lexical-binding: t -*-
-;;; init-js.el --- stuff related to Javascript
+;;; init-js.el --- stuff related to Javascript/Typescript/TSX
 
 (use-package nvm
   :commands (nvm-use nvm-use-for nvm--installed-versions))
 
-(use-package js-comint
-  :config
-  (add-hook 'js-ts-mode-hook
-            (lambda ()
-              (local-set-key (kbd "C-x C-e") 'js-send-last-sexp)
-              (local-set-key (kbd "C-M-x") 'js-send-last-sexp-and-go)
-              (local-set-key (kbd "C-c b") 'js-send-buffer)
-              (local-set-key (kbd "C-c C-b") 'js-send-buffer-and-go))))
-
-(use-package js2-mode
+(use-package typescript-ts-mode
   :init
   (progn
     (add-to-list
@@ -24,28 +15,22 @@
                        (let ((buff (current-buffer)))
                          (if (or (search-forward "React." nil t 1)
                                  (search-forward "import React" nil t 1))
-                             (js-ts-mode)
-                           (js-ts-mode))))))))
+                             (tsx-ts-mode)
+                           (typescript-ts-mode)))))))))
+
+(use-package js-comint
   :config
-  (setq-default js2-ignored-warnings '("msg.extra.trailing.comma"))
   (add-hook 'js-ts-mode-hook
-            (lambda()
-              (setq js-indent-level 2
-                    js2-basic-offset 2
-                    js2-highlight-level 3
-                    js2-bounce-indent-p t
-                    indent-tabs-mode nil
-                    js2-strict-missing-semi-warning nil)
-              ;; (tern-mode)
-              (js2-imenu-extras-mode)
-              (js2-refactor-mode)
-              (auto-revert-mode)
-              (flycheck-mode 1))))
+            (lambda ()
+              (local-set-key (kbd "C-x C-e") 'js-send-last-sexp)
+              (local-set-key (kbd "C-M-x") 'js-send-last-sexp-and-go)
+              (local-set-key (kbd "C-c b") 'js-send-buffer)
+              (local-set-key (kbd "C-c C-b") 'js-send-buffer-and-go))))
 
 (use-package add-node-modules-path
    :config
-   (add-hook 'js-ts-mode-hook 'add-node-modules-path)
-   (add-hook 'js-ts-mode-hook 'add-node-modules-path))
+   (add-hook 'typescript-ts-mode-hook 'add-node-modules-path)
+   (add-hook 'tsx-ts-mode-hook 'add-node-modules-path))
 
 (use-package prettier
   :hook
@@ -53,95 +38,9 @@
   :config
   (setq prettier-mode-sync-config-flag nil))
 
-;; :hook
-;;   (js2-mode . prettier-mode))
-
-;; (use-package eslint-fix
-;;   :config
-;;   (eval-after-load 'js2-mode
-;;     '(add-hook 'js2-mode-hook
-;;                (lambda()
-;;                  (add-hook 'after-save-hook 'eslint-fix nil t)))))
-
 (use-package eslintd-fix
-  :config
-  (if (eq system-type 'darwin)
-      (setq eslintd-fix-executable "/Users/sullie09/.nvm/versions/node/v16.19.1/bin/eslint_d")
-    (setq eslintd-fix-executable "/home/linuxbrew/.linuxbrew/bin/eslint"))
-  (eval-after-load 'js-ts-mode
-    '(add-hook 'js-ts-mode-hook 'eslintd-fix-mode)))
+  :hook ((typescript-mode . eslintd-fix-mode)
+         (tsx-ts-mode . eslintd-fix-mode)))
 
-(use-package js2-refactor
-  :after (js-ts-mode hydra)
-  :hook
-  (js-ts-mode . js2-refactor-mode)
-
-  :config
-  (js2r-add-keybindings-with-prefix "C-c C-r")
-
-  (require 'eds)
-  (require 'init-hydra)
-
-  ;; Hydra / js2
-  (defhydra js2-refactor-hydra (:color blue :hint nil)
-    "
-      Javascript
-
-  ^Functions^                    ^Variables^               ^Buffer^                      ^sexp^               ^Debugging^
-  ------------------------------------------------------------------------------------------------------------------------------
-  _lp_: Localize Parameter      _ev_: Extract variable   _wi_: Wrap buffer in IIFE    _k_:  js2 kill      _lt_: log this
-  _ef_: Extract function        _iv_: Inline variable    _ig_: Inject global in IIFE  _ss_: split string  _dt_: debug this
-  _ip_: Introduce parameter     _rv_: Rename variable    _ee_: Expand node at point   _sl_: forward slurp
-  _em_: Extract method          _vt_: Var to this        _cc_: Contract node at point _ba_: forward barf
-  _ao_: Arguments to object     _sv_: Split var decl.    _uw_: unwrap
-  _tf_: Toggle fun exp and decl _ag_: Add var to globals
-  _ta_: Toggle fun expr and =>  _ti_: Ternary to if
-"
-    ("ee" js2r-expand-node-at-point)
-    ("cc" js2r-contract-node-at-point)
-    ("ef" js2r-extract-function)
-    ("em" js2r-extract-method)
-    ("tf" js2r-toggle-function-expression-and-declaration)
-    ("ta" js2r-toggle-arrow-function-and-expression)
-    ("ip" js2r-introduce-parameter)
-    ("lp" js2r-localize-parameter)
-    ("wi" js2r-wrap-buffer-in-iife)
-    ("ig" js2r-inject-global-in-iife)
-    ("ag" js2r-add-to-globals-annotation)
-    ("ev" js2r-extract-var)
-    ("iv" js2r-inline-var)
-    ("rv" js2r-rename-var)
-    ("vt" js2r-var-to-this)
-    ("ao" js2r-arguments-to-object)
-    ("ti" js2r-ternary-to-if)
-    ("sv" js2r-split-var-declaration)
-    ("ss" js2r-split-string)
-    ("uw" js2r-unwrap)
-    ("lt" js2r-log-this)
-    ("dt" js2r-debug-this)
-    ("sl" js2r-forward-slurp)
-    ("ba" js2r-forward-barf)
-    ("k" js2r-kill)
-    ("q" nil)))
-
-
-(defhydra hydra-my-javascript-macros (:color blue :hint nil)
-    "
-^Testing^                        ^Refactoring^
--------------------------------------------------------
-_c_: insert a test case          _r_: refactoring menu
-_t_: go to test/implementation
-
-_q_: quit this menu
-    "
-    ("c" (eds/insert-enzyme-test-case t))
-    ("t" (eds/toggle-test-implementation))
-    ("r" (progn
-           (js2-refactor-hydra/body)
-           (hydra-push '(hydra-my-javascript-macros/body))))
-    ("q" nil))
-
-(eval-after-load 'js2-mode
-  '(key-chord-define js-ts-mode-map "MM" 'hydra-my-javascript-macros/body))
 
 (provide 'init-js)
