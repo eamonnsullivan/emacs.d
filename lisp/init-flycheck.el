7855a950df2fbc52cbdf69b82376d2a5a69f09af
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2026-02-03 19:36:38 +0000

    Simplify flycheck config.

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -5,33 +5,11 @@
   :after flycheck)
 
 (use-package flycheck
-  :diminish flycheck-mode
+  :straight t
   :config
-  ;; disable jshint since we prefer eslint checking
-  (setq-default flycheck-disabled-checkers
-                (append flycheck-disabled-checkers
-                        '(javascript-jshint)))
-  ;; use eslint with js2-mode for js and JSX files
-  (flycheck-add-mode 'javascript-eslint 'js2-mode)
-  (flycheck-add-mode 'javascript-eslint 'rjsx-mode)
-  (if (eq system-type 'darwin)
-      (setq flycheck-javascript-eslint-executable "/Users/sullie09/.nvm/versions/node/v16.19.1/bin/eslint_d")
-    (setq flycheck-javascript-eslint-executable "/home/eamonn/.nvm/versions/node/v16.19.1/bin/eslint_d"))
-  ;; add scalastyle for scala mode.
   (flycheck-add-mode 'scala-scalastyle 'scala-ts-mode)
-  ;; customize flycheck temp file prefix
-  (setq-default flycheck-temp-prefix ".flycheck")
-  ;; disable json-jsonlist checking for json files
-  (setq-default flycheck-disabled-checkers
-                (append flycheck-disabled-checkers
-                        '(json-jsonlist)))
-  (setq-default flycheck-disabled-checkers
-                (append flycheck-disabled-checkers
-                        '(emacs-lisp-checkdoc)))
-  (setq-default flycheck-scalastylerc "/home/eamonn/git/disco-api/scalastyle-config.xml")
-  (eval-after-load 'flycheck
-    '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
-  :init   (global-flycheck-mode))
+  :init
+  (global-flycheck-mode))
 
 (use-package flycheck-eglot
   :straight
