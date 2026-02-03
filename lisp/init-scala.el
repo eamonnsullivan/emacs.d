f0f0b04ca17e8ea834079fb81c44bd664cd9ae54
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2026-02-03 17:39:35 +0000

    Scala tweaks.

--- lisp/init-scala.el
+++ lisp/init-scala.el
@@ -1,18 +1,6 @@
 ;;; -*- lexical-binding: t -*-
 ;;; init-scala.el --- stuff related to coding in scala
 
-(require 'eds)
-
-(defun is-assignment-thing-p (line)
-  "Returns t if a line appears to be a def, val or var assignment. Nil otherwise."
-  (and (string-match-p "=" line) (or (string-match-p "def " line)
-                                     (string-match-p "val " line)
-                                     (string-match-p "var " line))))
-
-(defun has-annotation-p (line)
-  "Returns t if the line appears to already have a type annotation. Nil otherwise."
-  (string-match-p "\:[^)]+\=" line))
-
 (use-package scala-ts-mode
   :mode (("\.scala$" . scala-ts-mode)))
 
@@ -20,13 +8,7 @@
   :straight
   (sbt-mode :type git :host github :repo "hvesalai/emacs-sbt-mode")
   :commands sbt-start sbt-command
-  :bind (("C-c C-b" . sbt-hydra))
-  :config
-  ;; WORKAROUND allows using SPACE when in the minibuffer
-  (substitute-key-definition
-   'minibuffer-complete-word
-   'self-insert-command
-   minibuffer-local-completion-map))
+  :bind (("C-c C-b" . sbt-hydra)))
 
 ;; feature-mode
 (use-package feature-mode
