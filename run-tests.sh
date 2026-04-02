1a3542437f60b91e66f5f053711fce6bd07170f5
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2026-04-02 18:23:18 +0100

    Still trying to get undercover.el to work.

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 UNDERCOVER_FORCE=true eask test buttercup
+# UNDERCOVER_FORCE=true eask -q exec buttercup -L .
--- tests/undercover-init.el
+++
@@ -1,7 +0,0 @@
-;; -*- lexical-binding: t; -*-
-
-(when (require 'undercover nil t)
-  (undercover "lisp/eds-*.el"
-              (:report-file "coverage/.resultset.json")
-              (:report-format 'simplecov)
-              (:send-report nil)))
