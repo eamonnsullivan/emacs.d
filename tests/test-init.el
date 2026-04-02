f2aae22c1cd3f627ab5f515781b4542e2cf47562
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2026-04-02 15:44:12 +0100

    Try relative paths.

--- tests/test-init.el
+++ tests/test-init.el
@@ -2,15 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (setq undercover-force-coverage t)
-;; (when (require 'undercover nil t)
-;;   (undercover "lisp/eds-*.el"
-;;               (:report-file "coverage/.resultset.json")
-;;               (:report-format 'simplecov)
-;;               (:send-report nil)))
-
 ;; 1. Set up undercover FIRST, before any source files are loaded
-(load-file "./tests/undercover-init.el")
+(load-file "tests/undercover-init.el")
 
 ;; 2. Load the source files to be instrumented
 (require 'eds-blog)
--- tests/undercover-init.el
+++ tests/undercover-init.el
@@ -1,7 +1,7 @@
 ;; -*- lexical-binding: t; -*-
 
 (when (require 'undercover nil t)
-  (undercover "./lisp/eds-*.el"
-              (:report-file "./coverage/.resultset.json")
+  (undercover "lisp/eds-*.el"
+              (:report-file "coverage/.resultset.json")
               (:report-format 'simplecov)
               (:send-report nil)))
